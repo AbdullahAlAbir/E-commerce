fdb8bf98c4b12b7556822f88af4535faf5580133
Abdullah Al  Abir <abdullah.al.abir@g.bracu.ac.bd>
2022-08-23 02:28:07 +0600

    Add files via upload

---
+++ context_processors.py
@@ -0,0 +1,5 @@
+from cart.view.cart import Cart
+
+
+def cart(request):
+    return {'cart': Cart(request)}
---
+++ forms.py
@@ -0,0 +1,47 @@
+from django import forms
+
+# Import for editing the User forms
+from shop.model.models import Profile, Comment
+
+# Add the user form part
+from django.contrib.auth.models import User
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clear_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
+
+
+# Forms for editing the user personal details
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
+
+
+# Dynamic comment form
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'body')
